bbe1091431a463ce3c380c0e2c44e0f6ee34de88
TalanetsLiza <yarmantovich1999@mail.ru>
2023-06-26 15:48:01 +0300

    feat: implement localStorage

--- src/store/busketReducer/busketSlice.js
+++ src/store/busketReducer/busketSlice.js
@@ -1,16 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
-import busketInitialState from "./busketInitialState";
+
+const getInitialState = () => {
+    const getState = localStorage.getItem("busket");
+
+    if (getState) {
+        const state = JSON.parse(getState);
+        return state;
+    }
+    return {
+        data: [],
+    };
+};
+
+const saveToLocalSrorage = (state) => {
+    const jsonState = JSON.stringify(state);
+    localStorage.setItem("busket", jsonState);
+};
 
 const busketSlice = createSlice({
     name: "busket",
-    initialState: {
-        data: busketInitialState,
-    },
+    initialState: getInitialState(),
     reducers: {
         add: (state, action) => {
             const id = action.payload;
             const newItem = { id, count: 1 };
             state.data.push(newItem);
+            saveToLocalSrorage(state);
             
         },
         increase: (state, action) => {
@@ -19,17 +34,23 @@ const busketSlice = createSlice({
             if (dataItem) {
                 dataItem.count++;
             };
+            saveToLocalSrorage(state);
         },
         decrease: (state, action) => {
             const id = action.payload;
             const dataItem = state.data.find((item) => item.id === id);
             if (dataItem) {
                 dataItem.count--;
-            };
+            }
+            if (dataItem.count === 0) {
+                state.data = state.data.filter((item) => item.id !== id);
+            }
+            saveToLocalSrorage(state);
         },
         remove: (state, action) => {
             const id = action.payload;
             state.data = state.data.filter((item) => item.id !== id);
+            saveToLocalSrorage(state);
         },
     },
 
